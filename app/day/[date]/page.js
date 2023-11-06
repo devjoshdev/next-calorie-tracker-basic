1819dca87bf46c2dbb02e2831b3273d57677d065
devjoshdev <jsinanan1@gmail.com>
2023-11-06 08:17:55 -0500

    Add Food component, incomplete

--- app/day/[date]/page.js
+++ app/day/[date]/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import MyDatePicker from "@/components/MyDatePicker";
 import DateBar from "@/components/DateBar";
+import Food from "@/components/Food";
 const validateYear = (year) => {
     return (Number.parseInt(year) && (Number.parseInt(year) >= 1980));
 };
@@ -20,6 +21,7 @@ export default function Day(props) {
             <div>
                 <MyDatePicker currentDate={focusedDate}/>
                 <DateBar currentDate={focusedDate}/>
+                <Food foodName="Cheeseburger" numCals={650} />
                 <h1>Date: {focusedDate}</h1>
                 {`<h1>Hi</h1>`}
                 <svg fill="#000000" height="50px" width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="-47.62 -47.62 571.45 571.45" xmlSpace="preserve" transform="rotate(0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <polygon points="476.213,223.107 57.427,223.107 151.82,128.713 130.607,107.5 0,238.106 130.607,368.714 151.82,347.5 57.427,253.107 476.213,253.107 "></polygon> </g></svg>
