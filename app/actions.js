3cd609435694fb44b9410816fd3e374ee0c075b9
devjoshdev <jsinanan1@gmail.com>
2023-11-12 13:17:55 -0500

    Set up for use of mysql

--- app/actions.js
+++ app/actions.js
@@ -3,3 +3,17 @@
 async function createFood(data) {
     
 }
+
+async function updateFood(data) {
+
+}
+
+async function deleteFood(data) {
+
+}
+
+async function getFoods(day) {
+
+}
+
+export { createFood, updateFood, deleteFood, getFoods };
--- app/day/[date]/page.js
+++ app/day/[date]/page.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import MyDatePicker from "@/components/MyDatePicker";
 import DateBar from "@/components/DateBar";
-import Food from "@/components/Food";
 import FoodList from "@/components/FoodList";
+import { getFoods } from "@/app/actions";
 const validateYear = (year) => {
     return (Number.parseInt(year) && (Number.parseInt(year) >= 1980));
 };
@@ -12,17 +12,13 @@ const validateMonth = (month) => {
 const validateDay = (day) => {
     return (Number.parseInt(day) && (Number.parseInt(day) > 0 && Number.parseInt(day) < 32));
 };
-const getFoodList = async (date) => {
-    const res = await fetch(`http://localhost:3001/days?date=${date}`, {cache: "no-store"});
-    if (!res.ok) throw new Error("Could not grab the data");
-    return res.json();
-};
+
 export default async function Day(props) {
     const dateRegEx = new RegExp("^\\d{4}-\\d{2}-\\d{2}$");
     const focusedDate = props.params.date;
     const [focusedYear, focusedMonth, focusedDay] = focusedDate.split("-");
     console.log(focusedDate);
-    const foodListJSON = await getFoodList(focusedDate);
+    const foodListJSON = await getFoods(focusedDate);
     console.log(foodListJSON);
     let foodListData = [];
     if (foodListJSON.length !== 0) {
@@ -34,9 +30,6 @@ export default async function Day(props) {
             <div>
                 <MyDatePicker currentDate={focusedDate}/>
                 <DateBar currentDate={focusedDate}/>
-                {/* <Food foodName="Cheeseburger" numCals={650} />
-                <Food foodName="Side of Queso" numCals={150}/>
-                <Food foodName="Oatmeal" numCals={400}/> */}
                 <FoodList foods={foodListData}/>
             </div>
         );
